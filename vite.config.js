58a1f451510ed8640045cb453ec6846f36cab213
Souravkr-00 <cash.souravkr12@gmail.com>
2025-05-15 18:17:32 +0530

    vite file change

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-            react(),
-            tailwindcss(),
-  ],
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
